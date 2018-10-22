0e1ee7208f8d722065346e882196a98ac624779e
boinkbr <fraga.fabio@gmail.com>
2018-10-22 15:18:02 -0300

    Inserido sniffer

---
+++ sniffer_iter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+############################################################
+# COMENTARIOS SOBRE O PROGRAMA AQUI:
+# COMENTARIOS SOBRE O PROGRAMA AQUI:
+# COMENTARIOS SOBRE O PROGRAMA AQUI:
+# COMENTARIOS SOBRE O PROGRAMA AQUI:
+# Setup baseado em distros Debian-Like e RedHat
+# Fabio Fraga Machado em DATA
+# fraga.fabio@gmail.com
+# https://github.com/boinkbr
+############################################################
