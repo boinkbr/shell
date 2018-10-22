98ba49ef187f534c1479f4124e97ff4e6227ac13
Fabio Fraga Machado <33349970+boinkbr@users.noreply.github.com>
2018-10-22 20:27:17 -0300

    Update sniffer_iter.sh

--- sniffer_iter.sh
+++ sniffer_iter.sh
@@ -11,7 +11,7 @@
 # https://github.com/boinkbr
 ############################################################
 
-curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer fc8814796e9a91dadc4cf954c4d946bf61c9dc304885a858c17aab66bd03b9b8" "https://api.digitalocean.com/v2/droplets?page=1&per_page=200" > ip_do.txt
+curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer API_KEY" "https://api.digitalocean.com/v2/droplets?page=1&per_page=200" > ip_do.txt
 
 cat ip_do.txt| jq -r . | grep "ip_address" | egrep -v "10.1|2604" | awk -F ":" '{print $2}'
 
