04bfcb665b15a696bb6dd5add4d34fdbdbc7cf68
Fabio Fraga Machado <33349970+boinkbr@users.noreply.github.com>
2018-11-01 11:24:34 -0300

    Create sed.sh

---
+++ sed.sh
@@ -0,0 +1,9 @@
+sed -i 's/TESTING = "1"/TESTING = "0"/g' /etc/csf/csf.conf
+sed -i 's/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,2077,2078,2079,2080,2082,2083,2086,2087,2095,2096"/TCP_IN = ""/g' /etc/csf/csf.conf
+sed -i 's/TCP_OUT = "20,21,22,25,37,43,53,80,110,113,443,587,873,993,995,2086,2087,2089,2703"/TCP_OUT = "0:65535"/g' /etc/csf/csf.conf
+sed -i 's/UDP_IN = "20,21,53"/UDP_IN = ""/g' /etc/csf/csf.conf
+sed -i 's/UDP_OUT = "20,21,53,113,123,873,6277,24441"/UDP_OUT = "0:65535"/g' /etc/csf/csf.conf
+sed -i 's/TCP6_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,2077,2078,2079,2080,2082,2083,2086,2087,2095,2096"/TCP6_IN = ""/g' /etc/csf/csf.conf
+sed -i 's/TCP6_OUT = "20,21,22,25,37,43,53,80,110,113,443,587,873,993,995,2086,2087,2089,2703"/TCP6_OUT = "0:65535"/g' /etc/csf/csf.conf
+sed -i 's/UDP6_IN = "20,21,53"/UDP6_IN = ""/g' /etc/csf/csf.conf
+sed -i 's/UDP6_OUT = "20,21,53,113,123,873,6277,24441"/UDP6_OUT = "0:65535"/g' /etc/csf/csf.conf
